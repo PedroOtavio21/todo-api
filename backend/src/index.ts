167e584dede377c1bae236c3dea9f83fe0be88d9
PedroOtavio21 <pedrootavio.bezerra14@gmail.com>
2026-05-13 11:49:54 -0300

    feat: adiciona rotas finais e incrementa auth em rotas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import { prisma } from './lib/prisma'
+import { authRouter } from './routes/auth'
 import { taskRouter } from './routes/tasks'
 import { errorHandler } from './middlewares/error-handler'
 
@@ -8,6 +9,7 @@ dotenv.config()
 
 const app = express()
 app.use(express.json())
+app.use('/api/v1', authRouter)
 app.use('/api/v1', taskRouter)
 app.use(errorHandler)
 
--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { TaskRepository } from "../repositories/TaskRepository";
 import { TaskService } from "../services/TaskService";
 import { TaskController } from "../controllers/TaskController";
+import { authMiddleware } from '../middlewares/auth-middleware';
 
 const taskRouter = Router()
 
@@ -9,10 +10,10 @@ const taskRepository = new TaskRepository()
 const taskService = new TaskService(taskRepository)
 const taskController = new TaskController(taskService)
 
-taskRouter.get('/tasks', taskController.index)
-taskRouter.post('/tasks', taskController.store)
-taskRouter.get('/tasks/:id', taskController.show)
-taskRouter.put('/tasks/:id', taskController.update)
-taskRouter.delete('/tasks/:id', taskController.delete)
+taskRouter.get('/tasks', authMiddleware, taskController.index)
+taskRouter.post('/tasks', authMiddleware, taskController.store)
+taskRouter.get('/tasks/:id', authMiddleware, taskController.show)
+taskRouter.put('/tasks/:id', authMiddleware, taskController.update)
+taskRouter.delete('/tasks/:id', authMiddleware, taskController.delete)
 
 export { taskRouter }
