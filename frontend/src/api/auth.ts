bb6f0980ba00cbbb2215ecc7b58d24db0cee18af
PedroOtavio21 <pedrootavio.bezerra14@gmail.com>
2026-05-14 14:38:24 -0300

    feat: adiciona funções ligadas a autenticação, tokens e tarefas

---
+++ frontend/src/api/auth.ts
@@ -0,0 +1,27 @@
+import api from './axios'
+import type { User }  from '../types'
+
+interface RegisterData {
+  email: string
+  password: string
+  name?: string
+}
+
+interface LoginData {
+  email: string
+  password: string
+}
+
+interface LoginResponse {
+  token: string
+}
+
+export async function register(data: RegisterData): Promise<User> {
+  const response = await api.post<User>('/register', data)
+  return response.data
+}
+
+export async function login(data: LoginData): Promise<LoginResponse> {
+  const response = await api.post<LoginResponse>('/login', data)
+  return response.data
+}
