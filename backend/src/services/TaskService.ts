62f3f3b28fb8bb4ca38ffdf8a30ac09aaf7328c4
PedroOtavio21 <pedrootavio.bezerra14@gmail.com>
2026-05-16 09:09:09 -0300

    fix: corrige assinatura de método e altera status em resposta

--- backend/src/services/TaskService.ts
+++ backend/src/services/TaskService.ts
@@ -7,21 +7,21 @@ export class TaskService {
     async getAll(userId: number, status?: TaskStatus) {
         return this.repository.findAll(userId, status)
     }
-    async getById(userId:number, id: number){
+    async getById(id: number, userId: number) {
         const task = await this.repository.findById(id, userId)
         if (!task) throw new HttpError(404, "Task not found!")
         return task
     }
-    async create(task: Omit<Task, "id" | "createdAt" | "updatedAt">){
+    async create(task: Omit<Task, "id" | "createdAt" | "updatedAt">) {
         if (task.status === "DONE") throw new HttpError(400, "Cannot create a task already done!")
         return this.repository.create(task)
     }
-    async update(userId: number, id: number, task: Partial<Omit<Task, "id" | "createdAt" | "updatedAt">>) {
-        await this.getById(userId, id)
-        return this.repository.update(id, userId,task)
+    async update(id: number, userId: number, task: Partial<Omit<Task, "id" | "createdAt" | "updatedAt">>) {
+        await this.getById(id, userId)
+        return this.repository.update(id, userId, task)
     }
-    async delete(userId: number, id: number){
-        await this.getById(userId, id)
+    async delete(id: number, userId: number) {
+        await this.getById(id, userId)
         return this.repository.delete(id, userId)
     }
 }
